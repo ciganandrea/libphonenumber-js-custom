f49abbcf12a521b0fdb773e23649d8ab3687ce6d
Andrea Cigana <ciganandrea@gmail.com>
2020-11-25 19:48:16 +0100

    Remove unused functions from "custom" cource files

--- custom/index.commonjs.js
+++ custom/index.commonjs.js
@@ -20,76 +20,8 @@ function parsePhoneNumberFromString() {
 exports = module.exports = parsePhoneNumberFromString
 exports['default'] = parsePhoneNumberFromString
 
-exports.ParseError = core.ParseError
 
-function parsePhoneNumberWithError() {
-	return call(core.parsePhoneNumberWithError, arguments)
-}
-
-// `parsePhoneNumber()` named export has been renamed to `parsePhoneNumberWithError()`.
-exports.parsePhoneNumber = parsePhoneNumberWithError
-exports.parsePhoneNumberWithError = parsePhoneNumberWithError
 
 // `parsePhoneNumberFromString()` named export is now considered legacy:
 // it has been promoted to a default export due to being too verbose.
 exports.parsePhoneNumberFromString = parsePhoneNumberFromString
-
-exports.findNumbers = function findNumbers() {
-	return call(core.findNumbers, arguments)
-}
-
-exports.searchNumbers = function searchNumbers() {
-	return call(core.searchNumbers, arguments)
-}
-
-exports.findPhoneNumbersInText = function findPhoneNumbersInText() {
-	return call(core.findPhoneNumbersInText, arguments)
-}
-
-exports.searchPhoneNumbersInText = function searchPhoneNumbersInText() {
-	return call(core.searchPhoneNumbersInText, arguments)
-}
-
-exports.PhoneNumberMatcher = function PhoneNumberMatcher(text, options) {
-	return core.PhoneNumberMatcher.call(this, text, options, metadata)
-}
-exports.PhoneNumberMatcher.prototype = Object.create(core.PhoneNumberMatcher.prototype, {})
-exports.PhoneNumberMatcher.prototype.constructor = exports.PhoneNumberMatcher
-
-exports.AsYouType = function AsYouType(country) {
-	return core.AsYouType.call(this, country, metadata)
-}
-exports.AsYouType.prototype = Object.create(core.AsYouType.prototype, {})
-exports.AsYouType.prototype.constructor = exports.AsYouType
-
-exports.isSupportedCountry = function isSupportedCountry(country) {
-	return call(core.isSupportedCountry, arguments)
-}
-
-exports.getCountries = function getCountries() {
-	return call(core.getCountries, arguments)
-}
-
-exports.getCountryCallingCode = function getCountryCallingCode() {
-	return call(core.getCountryCallingCode, arguments)
-}
-
-exports.getExtPrefix = function getExtPrefix(country) {
-	return call(core.getExtPrefix, arguments)
-}
-
-exports.getExampleNumber = function getExampleNumber() {
-	return call(core.getExampleNumber, arguments)
-}
-
-exports.formatIncompletePhoneNumber = function formatIncompletePhoneNumber() {
-	return call(core.formatIncompletePhoneNumber, arguments)
-}
-
-exports.parseIncompletePhoneNumber = core.parseIncompletePhoneNumber
-exports.parsePhoneNumberCharacter = core.parsePhoneNumberCharacter
-exports.parseDigits = core.parseDigits
-exports.DIGIT_PLACEHOLDER = core.DIGIT_PLACEHOLDER
-
-exports.parseRFC3966 = core.parseRFC3966
-exports.formatRFC3966 = core.formatRFC3966
